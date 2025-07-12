c02899dbbb944453c693085dadd60c4d2063a394
Sam <90007553+Scherso@users.noreply.github.com>
2025-07-12 17:00:05 -0400

    Update health.rs

--- src/handler/health.rs
+++ src/handler/health.rs
@@ -1,7 +1,11 @@
 //! # Health check handler module.
 
 use axum::Json;
-use serde_json::{json, Value};
+use serde_json::{
+    json, 
+    Value
+};
+
 use crate::constant;
 use crate::handler::result::ResultHandler;
 
@@ -9,10 +13,10 @@ use crate::handler::result::ResultHandler;
 /// 
 /// # Returns:
 /// * `Json<Value>` - A JSON object containing the 
-///                       - health status, 
-///                       - service name, 
-///                       - version,
-///                       - current timestamp.
+///                   - health status, 
+///                   - service name, 
+///                   - version,
+///                   - current timestamp.
 pub async fn health_check() -> ResultHandler<Json<Value>> {
     Ok(Json(json!({
         "status":    constant::STATUS_OK,
