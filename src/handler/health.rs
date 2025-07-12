818cf43142234f08230584d75a3045a0b728acc8
Sam <90007553+Scherso@users.noreply.github.com>
2025-07-12 17:01:21 -0400

    Update health.rs

--- src/handler/health.rs
+++ src/handler/health.rs
@@ -12,11 +12,11 @@ use crate::handler::result::ResultHandler;
 /// Health check endpoint.
 /// 
 /// # Returns:
-/// * `Json<Value>` - A JSON object containing the 
-///                 - health status, 
-///                 - service name, 
-///                 - version,
-///                 - current timestamp.
+/// * `Json<Value>`: A JSON object containing the 
+///                  - health status, 
+///                  - service name, 
+///                  - version,
+///                  - current timestamp.
 pub async fn health_check() -> ResultHandler<Json<Value>> {
     Ok(Json(json!({
         "status":    constant::STATUS_OK,
